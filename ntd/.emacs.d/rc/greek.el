e4694e3a869db2713967196f59f232fa741f12f2
Neil T. Dantam <ntd@gatech.edu>
2014-07-31 20:04:58 -0400

    Greek key bindings

--- ntd/.emacs.d/rc/greek.el
+++ ntd/.emacs.d/rc/greek.el
@@ -46,6 +46,45 @@
 
 (add-hook 'pretty-greek-mode-hook 'pretty-greek)
 
+(defun ucs-code (name)
+  (cdr (assoc name (ucs-names))))
+
+
+(defun def-small-greek (key name)
+  (lexical-let ((code (ucs-code (concat "GREEK SMALL LETTER " (upcase name)))))
+    (global-set-key (concat "\C-cg" key)
+                    (lambda ()
+                      (interactive)
+                      (insert (make-string 1 code))))))
+
+
+(let ((greeks '(("a" "alpha")
+                ("b" "beta")
+                ("g" "gamma")
+                ("d" "delta")
+                ("e" "epsilon")
+                ("z" "zeta")
+                ("h" "eta")
+                ("j" "theta")
+                ("i" "iota")
+                ("k" "kappa")
+                ("l" "lambda")
+                ("m" "mu")
+                ("n" "nu")
+                ("x" "xi")
+                ("o" "omicron")
+                ("p" "pi")
+                ("r" "rho")
+                ("s" "sigma")
+                ("t" "tau")
+                ("y" "upsilon")
+                ("f" "phi")
+                ("c" "chi")
+                ("q" "psi")
+                ("w" "omega"))))
+  (loop for (key name) in greeks
+        do (def-small-greek key name)))
+
 
 ;(add-hook 'lisp-mode-hook 'pretty-greek-mode)
 ;(add-hook 'f90-mode-hook 'pretty-greek-mode)
