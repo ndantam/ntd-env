f12828cf25521e6917c2b2264de35cbda80be108
Neil T. Dantam <ntd@rice.edu>
2015-03-23 18:15:27 -0500

    Slime keybindings for asdf loads

--- ntd/.emacs.d/rc/cl.el
+++ ntd/.emacs.d/rc/cl.el
@@ -12,3 +12,7 @@
 (add-to-list 'auto-mode-alist '("\\.sbclrc$" . lisp-mode))
 (add-to-list 'auto-mode-alist '("\\.asdf-install$" . lisp-mode))
 (add-to-list 'auto-mode-alist '("\\.asd$" . lisp-mode))
+
+
+(global-set-key "\C-cll" 'slime-load-system)
+(global-set-key "\C-clr" 'slime-reload-system)
