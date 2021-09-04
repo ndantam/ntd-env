16af9ffeb4ffd8ad6264519ddb77a2ec88609b44
Neil T. Dantam <ndantam@mines.edu>
2021-09-03 19:11:40 -0600

    Improve the proxy script

--- proxy.js
+++ proxy.js
@@ -1,10 +1,17 @@
 function FindProxyForURL (url, host) {
-    // Mines URLS need the proxy
-    if (shExpMatch(host, '*.mines.edu')
-        || shExpMatch(host, '*.mines.edu:*') ) {
-        return 'SOCKS localhost:1080';
+    var proxy='SOCKS localhost:1080';
+    var domains = [
+        // Mines URLS get proxied
+        '.mines.edu',
+        // Proxy publication sites to get Mines access
+        'scholar.google.com',
+        'ieeexplore.ieee.org'
+    ];
+
+    if (domains.find( domain => dnsDomainIs(host, domain))) {
+        return proxy;
     }
 
-    // Don't proxy the whole intarweb
+    // Don't proxy the whole intarwebs
     return 'DIRECT';
 }
